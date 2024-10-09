4e5dc2c1a9356b2ee653f8f61432d8b05f27114c
programming-study-lab <anaphat.wo@rmuti.ac.th>
2024-10-10 00:10:00 +0700

    add some features

--- delete_comment.php
+++ delete_comment.php
@@ -8,11 +8,12 @@ $bufferTopicID = $_GET['topicID'];
 $topicID = "topicID=".$bufferTopicID;
 if ($_GET) {
     include "dblink.php";
-    $stmt = $link->prepare("SELECT * FROM comment");
+    $stmt = $link->prepare("SELECT * FROM comment WHERE CommentID = ?");
+            $stmt->bind_param("s", $_GET['commentID'] );
             $stmt->execute();
             $res = $stmt->get_result();
             $row = $res->fetch_assoc();
-    if ($row['UserID'] == $_SESSION['ID']) {
+    if ($row['UserID'] == $_GET['ID']) {
         $stmt = $link->prepare("DELETE FROM comment WHERE UserID = ? AND CommentID = ?");
         $stmt->bind_param("ss", $_SESSION['ID'], $_GET['commentID'] );
         $stmt->execute();
@@ -23,12 +24,14 @@ if ($_GET) {
         $stmt->bind_param("s", $_GET['commentID'] );
         $stmt->execute();
         header("Location: comment.php?$topicID");
+        // header("refresh: 3; url=comment.php?$topicID");
     } else {
         $message = "มีข้อผิดพลาดในการลบข้อมูล";
+        echo "<script>alert('$message')</script>";
         header("Location: comment.php?$topicID");
+        // header("refresh: 3; Location=comment.php?$topicID");
     }
     // echo $_SERVER['QUERY_STRING'];
-    echo "<script>alert('$message')</script>";
 }   
 
 ?>
